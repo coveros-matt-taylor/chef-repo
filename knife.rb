ad5d231fd098afcde688ba53d2f88cb3f2de2442
matt.taylor <matthew.taylor@coveros.com>
2018-03-27 13:41:17 -0400

    update knife.rb

--- knife.rb
+++ knife.rb
@@ -1,2 +1,5 @@
 # Amazon AWS
 knife[:aws_credential_file] = "/home/centos/aws.txt"
+# set current path and path to cookbooks
+current_dir = File.dirname(__FILE__)
+cookbook_path ["#{current_dir}/cookbooks"]

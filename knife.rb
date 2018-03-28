5ae507ecd71a6ae885e4244ddf2779f7cc6348d4
matt.taylor <matthew.taylor@coveros.com>
2018-03-28 09:42:31 -0400

    debug

--- knife.rb
+++ knife.rb
@@ -1,5 +1,8 @@
 # Amazon AWS
 knife[:aws_credential_file] = "/home/centos/aws.txt"
 # set current path and path to cookbooks
-current_dir = File.dirname(__FILE__)
-cookbook_path ["#{current_dir}/cookbooks"]
+# current_dir = File.dirname(__FILE__)
+# cookbook_path ["#{current_dir}/cookbooks"]
+cookbook_path ['~/chef-repo/cookbooks']
+client_key '~/chef-repo/.chef/aws.txt'
+chef_server_url `https://172.31.2.181/coveros_test/mtaylor`
